4d69a294c865386f14f7c79a95bea26593eabc6c
jovanylemoupa <jovanylemoupa@gmail.com>
2025-03-11 11:01:27 +0100

    Mise à jour des fichiers README.md, index.tsx et ajout du dossier backend

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Bonjour la Teams je fais ce teste pour montrer que le code fonctionne parfaiement </ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
